f2eb6977c8ffefaad0c328ac15016ef13d76397c
silvanalope <silvialopez@unisangil.edu.co>
2022-07-06 17:34:21 -0500

    changue script package

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload misi.
+          Edit <code>src/App.js</code> and save to reload misi pooo.
         </p>
         <a
           className="App-link"
