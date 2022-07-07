829df21921537899b106ff593033eff210c52351
silvanalope <silvialopez@unisangil.edu.co>
2022-07-07 14:34:58 -0500

    empli1

---
+++ src/ejercicios/uno.js
@@ -0,0 +1,17 @@
+export default function TodoList() {
+    return (
+      <>
+        <h1>Hedy Lamarr's Todos</h1>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzgtHL5npnrCVUhu0dsr59TiwlwKjoFyunJdV7a51O18X_XipizJHWXfQNifLYiYdqIz0&usqp=CAU"
+          alt="Yato Noragami"
+          className="photo"
+        />
+        <ul>
+          <li>personaje de ficcion</li>
+          <li>es un dios de rango menor</li>
+          <li>su gran sueño es tener un santuario</li>
+        </ul>
+      </>
+    );
+  }
